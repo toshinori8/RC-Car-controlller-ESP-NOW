728e72fcae7ded954a823e3c1a396a9d8f3e76c9
toshinori8 <zbigniew.karski@gmail.com>
2022-05-01 12:19:27 +0200

    smooth analog input on servo.

--- nodeMCU_reciver_src/LIGHTS.ino
+++ nodeMCU_reciver_src/LIGHTS.ino
@@ -1,41 +1,37 @@
 
-int counter_brake =6; // delay for stop led. 
+  int counter_brake = 6;  // delay for stop led.
 
-void  turnLightFW(int direction){
-Serial.println(direction);
-Serial.println(counter_brake);
-    if(direction==1){
-        // stop light       OFF
-        // reverse light    OFF 
-
+  void turnLightFW(int direction) {
+    Serial.println(direction);
+    Serial.println(counter_brake);
+    if (direction == 1) {
+      // stop light       OFF
+      // reverse light    OFF
     }
-     if(direction==2){
-        // stop light       ON
-        // Serial.println('stop    ON');
-        
-        if(counter_brake==0){
-                Serial.println("reverse ON");
-                Serial.println("stop    OFF");
-                // reverse light    ON 
-                // stop light       OFF
-        }else{
-          counter_brake--;
+    if (direction == 2) {
+      // stop light       ON
+      // Serial.println('stop    ON');
 
-        }
+      if (counter_brake == 0) {
+      //  Serial.println("reverse ON");
+      //  Serial.println("stop    OFF");
+        // reverse light    ON
+        // stop light       OFF
+      } else {
+        counter_brake--;
+      }
 
 
-        
-        
-    }else(counter_brake = 6);
 
-}
 
-void turnLightLeft(){
-// Serial.println('turn  LEFT');
+    } else
+      (counter_brake = 6);
+  }
 
-};
-void turnLightRight(){
+  void turnLightLeft() {
+  //  Serial.println('turn  LEFT');
+  };
+  void turnLightRight() {
 
-// Serial.println('turn  RIGHT');
-
-};
+   // Serial.println('turn  RIGHT');
+  };
--- nodeMCU_reciver_src/nodeMCU_reciver_src.ino
+++ nodeMCU_reciver_src/nodeMCU_reciver_src.ino
@@ -1,12 +1,15 @@
 
 // 9C:1F:46:63:3B  MAC ADRESS OF this device
 #include <ESP8266WiFi.h>
+#include <AnalogSmooth.h>
+
 //#include <WiFiClient.h>
 //#include <ESPAsyncWebServer.h>
 //#include <ESP8266mDNS.h>
 //#include <ESPAsyncTCP.h>
-uint8_t reciverMac[] = {0xCC, 0x50, 0xE3, 0x56, 0xB7, 0x36};
+uint8_t reciverMac[] = { 0xCC, 0x50, 0xE3, 0x56, 0xB7, 0x36 };
 
+AnalogSmooth as = AnalogSmooth(6);
 
 
 //#include <WiFiUdp.h>
@@ -21,7 +24,7 @@ uint8_t reciverMac[] = {0xCC, 0x50, 0xE3, 0x56, 0xB7, 0x36};
 //#define SCREEN_WIDTH 128
 //#define SCREEN_HEIGHT 32
 //#define OLED_RESET -1
-//#define SCREEN_ADDRESS 0x3C 
+//#define SCREEN_ADDRESS 0x3C
 //Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 //
 
@@ -29,17 +32,17 @@ uint8_t reciverMac[] = {0xCC, 0x50, 0xE3, 0x56, 0xB7, 0x36};
 
 #include <Timers.h>
 
-#include <Servo.h> 
+#include <Servo.h>
 
 Servo myservo;
 
 // These are the pins used to control the motor shield
-#define DRIVE_MOTOR_POWER 3 //  D2 nodeMCU D2 
-#define DRIVE_MOTOR_DIRECTION 4     // D4 nodeMCU
-//#define STEER_MOTOR_POWER D1 // Motor A   gpio 5 
+#define DRIVE_MOTOR_POWER 3      //  D2 nodeMCU D2
+#define DRIVE_MOTOR_DIRECTION 4  // D4 nodeMCU
+//#define STEER_MOTOR_POWER D1 // Motor A   gpio 5
 //#define STEER_MOTOR_DIRECTION D3 // gpio0
 
-// ANALOG INPUT 
+// ANALOG INPUT
 const int pot = 0;
 
 const char minAngle = 0;
@@ -52,7 +55,7 @@ const char maxAngle = 100;
 typedef struct dataStruct {
 
   /// CAR STERING DATA
-  
+
   int pot;
   int drive;
   int ster;
@@ -69,58 +72,55 @@ typedef struct dataStruct {
   bool alarm_lights;
   bool error_lights;
 
-  
-  
+
+
 } dataStruct;
 dataStruct dataBeam;
-  
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-int curR;
-int curL; 
-  
+
+void OnDataRecv(uint8_t* mac, uint8_t* incomingData, uint8_t len) {
+  int curR;
+  int curL;
+
   memcpy(&dataBeam, incomingData, sizeof(dataBeam));
 
-//Serial.println("RECIVIG DATA ON ESP_NOW");
-  
-//  Serial.print("Ster: ");
- // Serial.println(dataBeam.pot);
-//  Serial.print("Drive: ");
-//  Serial.println(dataBeam.drive);
-// Serial.print("Ster: ");
-//  Serial.println(dataBeam.dirSter);
-//  Serial.print("Drive: ");
- // Serial.println(dataBeam.dirDrive);
+  //Serial.println("RECIVIG DATA ON ESP_NOW");
+
+  //  Serial.print("Ster: ");
+  // Serial.println(dataBeam.pot);
+  //  Serial.print("Drive: ");
+  //  Serial.println(dataBeam.drive);
+  // Serial.print("Ster: ");
+  //  Serial.println(dataBeam.dirSter);
+  //  Serial.print("Drive: ");
+  // Serial.println(dataBeam.dirDrive);
 
 
-if(dataBeam.dirDrive=="^"){
-  digitalWrite(DRIVE_MOTOR_DIRECTION, LOW);
-  turnLightFW(1);
+  if (dataBeam.dirDrive == "^") {
+    digitalWrite(DRIVE_MOTOR_DIRECTION, LOW);
+    turnLightFW(1);
   }
-if(dataBeam.dirDrive=="V"){
-  digitalWrite(DRIVE_MOTOR_DIRECTION, HIGH);
- turnLightFW(2);
+  if (dataBeam.dirDrive == "V") {
+    digitalWrite(DRIVE_MOTOR_DIRECTION, HIGH);
+    turnLightFW(2);
   }
-   analogWrite(DRIVE_MOTOR_POWER, dataBeam.drive*6);
- //int servos = map(dataBeam.pot, 754,427, 75, 106 );
-int servos = map(dataBeam.pot, 754,427, 0, 180 );
+  analogWrite(DRIVE_MOTOR_POWER, dataBeam.drive * 10);
+  //int servos = map(dataBeam.pot, 754,427, 75, 106 );
+  int servos = map(dataBeam.pot, 754, 427, 0, 180);
 
-//servos=servos*180.0/1023;
+  //servos=servos*180.0/1023;
 
- // Serial.println(servos);
+   Serial.println(dataBeam.drive);
 
-
-
-if(servos>70){
-turnLightLeft();
-}else if(servos<110){
-turnLightRight();
-}
-
-myservo.write(servos);
+  float analogSmooth = as.smooth(servos);
 
 
+  if (servos > 70) {
+    turnLightLeft();
+  } else if (servos < 110) {
+    turnLightRight();
+  }
 
-
+  myservo.write(analogSmooth);
 }
 
 
@@ -131,7 +131,7 @@ myservo.write(servos);
 
 
 
- 
+
 //AsyncWebServer server(80);
 
 String wifiMacString;
@@ -183,31 +183,31 @@ String inputMessage2;
 //
 //void displayData(void) {
 //  display.clearDisplay();
-//  display.setTextSize(1);       
-//  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);    
-//  display.setCrsor(0,10);          
+//  display.setTextSize(1);
+//  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+//  display.setCrsor(0,10);
 //  display.println(PARAM_INPUT_1);
 //
 //  display.setTextColor(SSD1306_WHITE);
-//  display.setCursor(0,20);           
+//  display.setCursor(0,20);
 //  display.println(inputMessage1);
 //
-//  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);        
-//  display.setCursor(50, 10);     
+//  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+//  display.setCursor(50, 10);
 //  display.print(PARAM_INPUT_2);
-//  display.setCursor(50, 10);  
-//  display.setTextColor(SSD1306_WHITE);   
+//  display.setCursor(50, 10);
+//  display.setTextColor(SSD1306_WHITE);
 //  display.print(inputMessage2);
-//  
+//
 //
 //  display.display();
-//           
 //
 //
 //
 //
 //
-//  
+//
+//
 //}
 
 
@@ -215,71 +215,71 @@ void initESP_NOW();
 
 
 
-void setup(void){
+void setup(void) {
   Serial.begin(115200);
   delay(1000);
-//analogWrite(15, 1023);
-    myservo.attach(2); // digital PIN D1 on nodeMCU / 
+  //analogWrite(15, 1023);
+  myservo.attach(2);  // digital PIN D1 on nodeMCU /
 
-      delay(200);
-    myservo.write(90);
+  delay(200);
+  //myservo.write(90);
 
- 
-      delay(200);
-          myservo.write(0);
 
-      delay(200);
-      
-    digitalWrite(LED_BUILTIN,HIGH ); 
-      
-      
-      myservo.write(180);
+  delay(200);
+ // myservo.write(0);
+
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+
+  myservo.write(180);
 
   //Wire.begin(D5, D6);
 
-  pinMode(DRIVE_MOTOR_POWER, OUTPUT);     
+  pinMode(DRIVE_MOTOR_POWER, OUTPUT);
   pinMode(DRIVE_MOTOR_DIRECTION, OUTPUT);
- // pinMode(STEER_MOTOR_POWER, OUTPUT);
-//STEER_MOTOR_DIRECTION  pinMode(STEER_MOTOR_DIRECTION, OUTPUT);
-  
+  // pinMode(STEER_MOTOR_POWER, OUTPUT);
+  //STEER_MOTOR_DIRECTION  pinMode(STEER_MOTOR_DIRECTION, OUTPUT);
+
   //analogWrite(STEER_MOTOR_POWER, 0);
   analogWrite(DRIVE_MOTOR_POWER, 0);
 
 
 
-  
-    /////////////////////////////////////     OLED DISOPLAY 
-  
-   
-//   
-//   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-//    Serial.println(F("SSD1306 allocation failed"));
-//    for(;;); // Don't proceed, loop forever
-//    }
-//  
 
- // displayData();
-     /////////////////////////////////////     OLED DISOPLAY 
+  /////////////////////////////////////     OLED DISOPLAY
 
 
+  //
+  //   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  //    Serial.println(F("SSD1306 allocation failed"));
+  //    for(;;); // Don't proceed, loop forever
+  //    }
+  //
+
+  // displayData();
+  /////////////////////////////////////     OLED DISOPLAY
 
 
-      
+
+
+
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(1000);
-  
- 
-//  WiFi.begin(ssid, password);
+
+
+  //  WiFi.begin(ssid, password);
 
 
   initESP_NOW();
 
   // Wait for connection
-//  while (WiFi.status() != WL_CONNECTED) {
-//    delay(200);
-//    Serial.print(".");
-//  }
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    delay(200);
+  //    Serial.print(".");
+  //  }
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
@@ -288,85 +288,77 @@ void setup(void){
   wifiMacString = WiFi.macAddress();
   Serial.print('MAC ADDRES :: ');
   Serial.println(wifiMacString);
-//  if (MDNS.begin("Rancho")) {
-//    
-//    Serial.println("MDNS Responder Started");
-//      //server.begin();
-//      //u8g2.begin();
-//  }
+  //  if (MDNS.begin("Rancho")) {
+  //
+  //    Serial.println("MDNS Responder Started");
+  //      //server.begin();
+  //      //u8g2.begin();
+  //  }
   //otaStart();
-  
 
-  
+
+
   //server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
- // request->send_P(200, "text/html", webpage, processor);
-//});
+  // request->send_P(200, "text/html", webpage, processor);
+  //});
 
   // server.on("/ajaxSter", [](AsyncWebServerRequest *request){
-        
-        
-        
-      //  if (request->hasParam(PARAM_INPUT_2)) {
-       //     inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-
-      //  }
-    //Serial.println(PARAM_INPUT_2);
-   // Serial.println(inputMessage2);
-
-   // displayData();
-   // request->send_P(200, "text/html", "ajaxSterOK");
-    
-   
- // });
-
-      
- // server.on("/ajaxDrive", [](AsyncWebServerRequest *request){
-
-    
-    
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-    //if (request->hasParam(PARAM_INPUT_1)) {
-     // inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
-     
-
-//Serial.println(PARAM_INPUT_1);
-//Serial.println(inputMessage1);
 
 
- // displayData();
-      
-   // }
+
+  //  if (request->hasParam(PARAM_INPUT_2)) {
+  //     inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+
+  //  }
+  //Serial.println(PARAM_INPUT_2);
+  // Serial.println(inputMessage2);
+
+  // displayData();
+  // request->send_P(200, "text/html", "ajaxSterOK");
+
+
+  // });
+
+
+  // server.on("/ajaxDrive", [](AsyncWebServerRequest *request){
+
+
+
+  // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+  //if (request->hasParam(PARAM_INPUT_1)) {
+  // inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+
+
+  //Serial.println(PARAM_INPUT_1);
+  //Serial.println(inputMessage1);
+
+
+  // displayData();
+
+  // }
 
   //  request->send_P(200, "text/html", "ajaxOK");
-    
-   
- // });
-  
-  
-//  server.on("/right", [](AsyncWebServerRequest *request){
-//    Serial.println("right");
-//    analogWrite(STEER_MOTOR_POWER, steeringPower);
-//    digitalWrite(STEER_MOTOR_DIRECTION, steerDirection);
-//    request->send_P(200, "text/html", "right");
-//  });
- 
 
-//   server.onNotFound(notFound);
 
- 
-//  Serial.println("HTTP Server Started");
+  // });
+
+
+  //  server.on("/right", [](AsyncWebServerRequest *request){
+  //    Serial.println("right");
+  //    analogWrite(STEER_MOTOR_POWER, steeringPower);
+  //    digitalWrite(STEER_MOTOR_DIRECTION, steerDirection);
+  //    request->send_P(200, "text/html", "right");
+  //  });
+
+
+  //   server.onNotFound(notFound);
+
+
+  //  Serial.println("HTTP Server Started");
 }
 
 
 
-void loop(void){
+void loop(void) {
   //ArduinoOTA.handle();
- 
-
-
-
- 
-
-
-  
 }
